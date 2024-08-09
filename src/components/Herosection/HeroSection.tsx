4a21237a323b889bffa98bf05515efa614d5db48
Bridget Amana <amanabridget03@gmail.com>
2024-08-09 02:33:04 +0100

    update hero section

--- src/components/Herosection/HeroSection.tsx
+++ src/components/Herosection/HeroSection.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import Logo from '../../assets/img/Logo.svg';
 import Stars from "../../assets/img/star.svg";
 import Clients from "../../assets/img/clients.png"
-import { IoMdReturnRight } from "react-icons/io";
+import { IoMdReturnRight, IoMdMenu, IoMdClose } from "react-icons/io";
+import { LuUserCheck2 } from "react-icons/lu";
 import "./herosection.scss";
 import Carscoup from '../../assets/img/carscoup-logo.svg';
 import Classpass from '../../assets/img/classpass-logo.svg';
@@ -14,10 +15,12 @@ import Reicapital from '../../assets/img/reicapital-logo.svg';
 import Spring from '../../assets/img/spring-logo.svg';
 import Square from '../../assets/img/square-logo.svg';
 
-
 const HeroSection = () => {
 
+    // Tag-loader
     const [isLoading, setLoading] = useState(true);
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -27,21 +30,54 @@ const HeroSection = () => {
         return () => clearTimeout(timer);
     }, []);
 
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
     return (
         <div>
-            <div className='stickynav'>
-                <div className='menu-link-container'>
-                    <a href="#about" className='menu-link'>About</a>
-                    <a href="#services" className="menu-link">Services</a>
-                    <a href="#work" className="menu-link">Work</a>
-                    <a href="#price" className="menu-link">Pricing</a>
-                    <a href="#faqs" className="menu-link">FAQs</a>
+            <div className="nav">
+                <div className='nav-wrapper'>
+                    <div className="navbar">
+                        <a href="#">
+                            <img src={Logo} alt="" />
+                        </a>
+                        <nav className={isMenuOpen ? 'open' : ''}>
+                            <ul className='nav-items'>
+                                <li><a href="#about">about</a></li>
+                                <li><a href="#about">services</a></li>
+                                <li><a href="#about">work</a></li>
+                                <li><a href="#about">pricing</a></li>
+                                <li><a href="#about">faqs</a></li>
+                                <li><a href="#about" className='yellow'>contact us</a></li>
+                            </ul>
+                            <button className={`toggle-button ${isMenuOpen ? 'exit' : 'menu'}`} onClick={toggleMenu}>
+                                <IoMdMenu className={`icon ${isMenuOpen ? 'hidden' : ''}`} />
+                                <IoMdClose className={`icon ${isMenuOpen ? '' : 'hidden'}`} />
+                            </button>
+                        </nav>
+                        <div className='menu-button' onClick={toggleMenu}>
+                            <IoMdMenu />
+                        </div>
+                        <div className="desktop-nav">
+                            <div>
+
+                            <a href="#about">About</a>
+                            <a href="#services">Services</a>
+                            <a href="#work">Work</a>
+                            <a href="#price">Pricing</a>
+                            <a href="#faqs">FAQs</a>
+                            </div>
+                        </div>
+                            <a href="#contact-us " className='yellow-nav-link'>Contact us
+                                <LuUserCheck2 />
+                            </a>
+                    </div>
                 </div>
             </div>
             <section className="hero-section">
-                <div className='logo-container'>
+                {/* <div className='logo-container'>
                     <img src={Logo} alt="Mapledesign Logo" />
-                </div>
+                </div> */}
                 <div className="title-container">
                     <span className="slot-tag">
                         <span className='dot'></span>
@@ -50,7 +86,7 @@ const HeroSection = () => {
                                 className={isLoading ? "" : "hidden"}
                             >Loading slots...</p>
                         ) : (
-                        <p>Only 2 spot left</p>
+                            <p>Only 2 spot left</p>
                         )}
                     </span>
                     <div className="title">
@@ -61,7 +97,7 @@ const HeroSection = () => {
                         Guiding ventures to brand eminence through innovative designs
                     </div>
                     <div className="button-wrapper">
-                        <a href="#">
+                        <a href="#" className='main-button'>
                             <div>
 
                                 <IoMdReturnRight />
@@ -81,6 +117,9 @@ const HeroSection = () => {
                     </div>
                 </div>
             </section>
+            <div className="trail">
+                <img src="" alt="" />
+            </div>
             <div className='companies-slider'>
                 <div>
                     <img src={Carscoup} alt="" className='logo-img_1' />
